5c028c3fd3c73613970e6c1751b12ba3a59686bb
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-31 23:59:29 +0000

    ejercicio 8

--- exercises/05-project-name/app.py
+++ exercises/05-project-name/app.py
@@ -1,3 +1,7 @@
 import requests
 
 # your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/project1.php")
+
+course = response.json()
+print(course['name'])
--- exercises/06-project-list/app.py
+++ exercises/06-project-list/app.py
@@ -1,3 +1,8 @@
 import requests
 
 # your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/project_list.php")
+
+project = response.json()
+
+print(project[1]['name'])
